c55d216d4039e88c30c8a3f70a0d2d5e761cb4c0
Matthew Hinea <matthew.hinea@gmail.com>
2021-01-27 20:09:20 -0800

    complete weather.py

--- consumers/models/weather.py
+++ consumers/models/weather.py
@@ -1,6 +1,6 @@
 """Contains functionality related to Weather"""
 import logging
-
+import json
 
 logger = logging.getLogger(__name__)
 
@@ -15,9 +15,7 @@ class Weather:
 
     def process_message(self, message):
         """Handles incoming weather data"""
-        logger.info("weather process_message is incomplete - skipping")
-        #
-        #
-        # TODO: Process incoming weather messages. Set the temperature and status.
-        #
-        #
+        # (DONE)TODO: Process incoming weather messages. Set the temperature and status.
+        json_data = json.loads(message.value())
+        self.temperature = json_data.get('temperature')
+        self.status = json_data.get('status')
